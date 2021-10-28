036ad92c08390b4bf33a88ac45e83161657b7e67
rapaka.chandrashekar@gmail.com <rapaka.chandrashekar@gmsil.com>
2021-10-29 01:39:56 +0530

    useContext part imp

--- src/Product.js
+++ src/Product.js
@@ -1,25 +1,45 @@
 import React from 'react'
 import './Product.css'
+import { useStateValue } from './StateProvider'
 
-function Product(props) {
+function Product({id,title,image,price,rating}) {
+const[{basket},dispatch]=useStateValue();
+console.log(basket);
+ 
+    //dispatch item data  to data layer
+const addToBasket=()=>{
+   console.log("inproduct");
+    dispatch({
+        type:'ADD_TO_BASKET',
+        item:{
+            id:id,
+            title:title,
+            image:image,
+            price:price,
+            rating:rating,
+        },
+
+    });
+
+};
     return (
         <div className="product">
             <div className="product_info">
-                <p>{props.title}</p>
+                <p>{title}</p>
                 <p className="product_price">
                     <small>$</small>
-                    <strong>{props.price}</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product_rating">
-                    {Array(props.rating).fill().map((_, i)=>(
+                    {Array(rating).fill().map((_, i)=>(
                     <p>⭐</p>
                      ))}
 
                 </div>
             </div>
            
-            <img src={props.image}></img>
-            <button>Add to Basket</button>
+            <img src={image}></img>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
 
     )
